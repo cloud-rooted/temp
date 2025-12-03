e3c53867d6cc0e31a86ba594e8d70a6ab89828d9
Adnan <adnanali23189@gmail.com>
2025-12-03 09:18:39 +0530

    feat: Add Set-9

---
+++ set-9/AccessSpecifierDemo.java
@@ -0,0 +1,41 @@
+class Student {
+    private String name;      // private member
+    private int marks;
+
+    // Public setter methods
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setMarks(int m) {
+        marks = m;
+    }
+
+    // Public getter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Marks: " + marks);
+    }
+}
+
+public class AccessSpecifierDemo {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        // Accessing private members via public methods
+        s.setName("Osman");
+        s.setMarks(95);
+
+        System.out.println("Student Name: " + s.getName());
+        System.out.println("Student Marks: " + s.getMarks());
+
+        s.display();
+    }
+}
